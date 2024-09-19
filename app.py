de9920c03a16757e5ca76f63238419f43320d01c
Abhijeet kumar <96976543+A7bhi9955@users.noreply.github.com>
2024-09-20 05:21:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import streamlit as st
 import pickle
 import numpy as np
+import os
+
+# Get the directory of the current file and form the model path
+model_path = os.path.join(os.path.dirname(__file__), 'models', 'village_healthcare_model.pkl')
 
 # Load the trained model
-with open('/Users/gadgetspro/Desktop/village_healthcare_model.pkl','rb') as file:
+with open(model_path, 'rb') as file:
     model = pickle.load(file)
 
 # Title of the web app
 st.title("Village Healthcare Access Prediction")
 
-# Add inputs for user to fill
+# Add inputs for the user to fill
 population = st.number_input("Enter Population", min_value=0)
 distance_hospital = st.number_input("Enter Distance to Nearest Hospital (in km)", min_value=0)
 health_centers = st.number_input("Enter Number of Health Centers", min_value=0)
@@ -26,4 +30,5 @@ if st.button("Predict Policy Impact Score"):
 
     # Display the result
     st.success(f"Predicted Policy Impact Score: {prediction[0]}")
+
 
